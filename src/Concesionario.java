56a4c693ae16cd48135f5c7d897684f827719ba1
Alejo <211151669+AlejoO-HdZ@users.noreply.github.com>
2025-05-10 17:37:54 -0500

    en el metodo realizarVenta se implementa null para evitar errores y corrige el codihoVehiculo y vehiculo.remove para mejorar la busqueda y eliminacion del vehiculo vendido, se agrega documentacion

--- src/Concesionario.java
+++ src/Concesionario.java
@@ -7,23 +7,30 @@ public class Concesionario {
 
     // Método para agregar un vehículo al inventario
     public void agregarVehiculo(Vehiculo vehiculo) {
-        ********.add(vehiculo);
+        inventario.add(vehiculo);
     }
 
     // Método para realizar una venta de un vehículo
+    /**
+     * codigoVehiculo Código único del vehículo
+     * monto Precio de la venta
+     * apellido Apellido del comprador
+     * nombre Nombre del comprador
+     * documento Documento de identificación del comprador
+     */
     public void realizarVenta(String codigoVehiculo, double monto, String apellido, String nombre, String documento) {
-        Vehiculo vehiculoVendido = *****;
-        // Buscar el vehículo en el inventario
+        Vehiculo vehiculoVendido = null;
+        // Buscar el vehículo en el inventario pos su codigo
         for (Vehiculo v : inventario) {
-            if (v.getCodigo().equals(codigo*******)) {
+            if (v.getCodigo().equals(codigoVehiculo)) {
                 vehiculoVendido = v;
                 break;
             }
         }
-        // Si se encuentra el vehículo, se registra la venta
+        // Si se encuentra el vehículo, se registra la venta y se elimina del inventario
         if (vehiculoVendido != null) {
             ventas.add(new Venta(monto, vehiculoVendido, apellido, nombre, documento));
-            *******.remove(vehiculoVendido);
+            inventario.remove(vehiculoVendido);
             System.out.println("Venta realizada: " + nombre + " " + apellido + " ha comprado " + vehiculoVendido.getMarca());
         } else {
             System.out.println("Error: Vehículo con código " + codigoVehiculo + " no encontrado.");
