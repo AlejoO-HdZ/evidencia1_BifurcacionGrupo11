ef587f6cde1cea2ebb325ee61647c961567199b9
Alejo <211151669+AlejoO-HdZ@users.noreply.github.com>
2025-05-11 18:54:48 -0500

    Ultimo Commit: Se incluye, en la documentacion del main, un comentario final a modo de conclusion del trabajo.

--- src/Main.java
+++ src/Main.java
@@ -19,3 +19,14 @@ public class Main {
         concesionario.mostrarVentas();
     }
 }
+
+/* Comentario Final (CONCLUSION)
+
+Finalmente en base a todo lo desarrollado en la actividad y los procedimientos aplicados podemos decir que hay diferentes formas de aprender peros siempre enriquecedoras,
+haciendo uso correcto de plataformas que mejoran nuestras habilidades y fortalecen los conocimientos dentro de lo que es el mundo de la programación, el uso de GitGub nos
+permitió explorar nuevas herramientas y opciones para programar y modificar código en lenguaje JAVA, aprendiendo y recordando términos y secuencias que podemos usar para crear
+y modificar funciones ejecutando de manera segura. Todo esto en función con la temática vista en las clases nos permite comprender que muchas plataformas con la IA, son
+herramientas que mejoran nuestras prácticas a la hora de trabajar con distintos lenguajes de programación y así mismo permiten ampliar nuestro conocimiento mejorando la calidad
+de aprendizaje ya sea de manera colectiva o individual, lo cual lo hace aún más interesante ya que aprovechando la herramienta como el internet podemos mejorar capacidades
+desde el aprendizaje explorando nuevas herramientas con un solo fin conocer, comprender y aprender.
+*/
